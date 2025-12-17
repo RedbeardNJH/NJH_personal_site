e71ec67b50c1898675e3116452548d4d01167400
RedbeardNJH <154090043+RedbeardNJH@users.noreply.github.com>
2025-12-17 17:20:27 -0500

    Personal Site Beginning
    Start of personal Site, Vite + react

---
+++ NJH_personal_site/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Header'
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <div class="image_container">
+          <img src="https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/e/ec/Progenitus.jpg"/>
+          <img />
+          <img />
+        </div>
+      </main>
+    </>
+  )
+}
---
+++ NJH_personal_site/src/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return(
+        <>
+        <header>
+            <h1>Noah J Harden</h1>
+        </header>
+        </>
+    )
+}
